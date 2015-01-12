e951ca8689f904f1d17771621aa5f4708f488f5a
Guillaume227 <guillaume.giraud@gmail.com>
2015-01-12 16:14:25 +0100

    ajout des dependences aura/pygapp dans setup.py

--- rosebot_description/scripts/goal_to_vel.py
+++ rosebot_description/scripts/goal_to_vel.py
@@ -10,6 +10,7 @@ from nav_msgs.msg import Odometry
 import math
 #from transform_utils import quat_to_angle
 import PyKDL
+from aura import OuvrirPartie, Vec
 
 def quat_to_angle(quat):
     rot = PyKDL.Rotation.Quaternion(quat.x, quat.y, quat.z, quat.w)
@@ -23,6 +24,13 @@ def prod_scal(u,v):
     return u[0]*v[0] + u[1]*v[1]
 def prod_vect(u,v):
     return u[0]*v[1] - u[1]*v[0]
+
+def coordsToVec(coords):
+    x,y = coords
+    return Vec(y*1000,-x*1000)
+
+def vecToCoords(v,scale=.001):
+    return -v.y*scale, v.x*scale
 
 class GoalToVel:
 
@@ -38,11 +46,18 @@ class GoalToVel:
         rospy.Subscriber('goal', Pose2D, self.goalCallback)
         rospy.Subscriber('odom', Odometry, self.odomCallback)
 
-
         self.rate = rospy.get_param("~rate", 50)
         self.timeout_ticks = rospy.get_param("~timeout_ticks", 2)
         self.goal = None
-        
+        #print 'init'
+        self.map = OuvrirPartie(nom='robomovies_vide.par')
+        #self.map.Affiche = False
+        self.map.Const_IPS = True
+        self.robot = iter(self.map.Selection).next()
+        self.map.InitPreBoucle()
+        self.boucleIter = self.map.boucleIter(exo_controle=True)
+        self.map.Courir = True
+        self.map.ImgParSec = self.rate # TODO set the rate based on elapsed time since last call for more accuracy
         
     #############################################################
     def spin(self):
@@ -75,34 +90,53 @@ class GoalToVel:
         twist.linear.z = 0
         twist.linear.x = 0
         twist.linear.y = 0
-                
-        vit_angle = 1.5
-        v_max = .5
+        
+        
+        pos = vecToCoords(self.robot.pos)
+        orient_vec = vecToCoords(self.robot.orient_,scale=1.)
+        prev_v = vecToCoords(self.robot._v_)
+        
+        self.boucleIter.next()
+        next_v = vecToCoords(self.robot._v_)
 
         if self.goal:
-            diff_goal = diff_vec(self.goal, self.pos)
-            dist_goal = norme(diff_goal)
 
-            if dist_goal > 1e-2:
-                goal_orient = diff_goal[0]/dist_goal, diff_goal[1]/dist_goal
-                orient_vec = math.cos(self.orient), math.sin(self.orient)
-                diff_orient = diff_vec(goal_orient, orient_vec)
+            if False:
+    
+                twist.linear.x = next_v.nor
+                twist.angular.z = math.atan2(prev_v ^ next_v, prev_v * next_v) * self.rate # angular velocity
+            
+            else:
                 
-                if norme(diff_orient) <= 1e-1:
-                    
-                    twist.linear.x = v_max #* math.cos(self.orient)
-                    #twist.linear.y = v_max * math.sin(self.orient)
-                    
-                    vit_angle = 0.1#min( vit_angle, diff_orient)
+                vit_angle = 1.5
+                v_max = .5
+            
+                diff_goal = diff_vec(self.goal, pos)
+                dist_goal = norme(diff_goal)
                 
-                if norme(diff_orient) >= 1e-2:
-
-                    # realign needed
-                    twist.angular.z = vit_angle * ( 1 if prod_vect(orient_vec,diff_goal) <= 0 else -1)
+                if dist_goal > 1e-2:
                     
-            elif abs(self.goal_orient - self.orient) <= 1e-3: 
-                twist.angular.z = vit_angle * ( 1 if (self.goal_orient - self.orient > 0) else -1)
- 
+                    goal_orient = diff_goal[0]/dist_goal, diff_goal[1]/dist_goal                
+                    diff_orient = diff_vec(goal_orient, orient_vec)
+                    
+                    if norme(diff_orient) <= 5e-1:
+                        
+                        twist.linear.x = v_max 
+                        
+                    if norme(diff_orient) <= 1e-1:
+                        vit_angle   = 0.1 #min( vit_angle, diff_orient)
+                    
+                    if norme(diff_orient) >= 1e-2:
+    
+                        # realign needed
+                        twist.angular.z = vit_angle * ( 1 if prod_vect(orient_vec,diff_goal) <= 0 else -1)
+                        
+                elif abs(self.goal_orient - self.orient) <= 1e-3: 
+                    twist.angular.z = vit_angle * ( 1 if (self.goal_orient - self.orient > 0) else -1)
+                
+                else:
+                    self.goal = None
+                    rospy.loginfo("-D- goal reached")
             
             self.pub_cmd_vel.publish(twist)
             
@@ -110,33 +144,35 @@ class GoalToVel:
     #############################################################
     def goalCallback(self,msg):
     #############################################################
-        rospy.loginfo("-D- goalCallback: %s" % str(msg))
         self.ticks_since_target = 0
         self.goal = [msg.x, msg.y]
         self.goal_orient = msg.theta
-        rospy.loginfo("-D- orient: %.2f w_vit %.2f" % (self.orient, self.w_vit))
-        rospy.loginfo("-D- pos   : %.2f,%.2f " % (self.pos[0], self.pos[1]))
+        #rospy.loginfo("-D- orient: %.2f w_vit %.2f" % (self.orient, self.w_vit))
+        #rospy.loginfo("-D- pos   : %.2f,%.2f " % (self.pos[0], self.pos[1]))
         #self.Path = self.mesh_map.pathFind_py(self.pos_est, self.goal))
+        self.robot.AssBut(coordsToVec(self.goal))
+        rospy.loginfo("-D- goalCallback: %s" % str(msg))
 
 
     #############################################################
     def odomCallback(self,msg):
-    #############################################################
+    #######################################
+        ######################
         #rospy.loginfo("-D- odomCallback: %s" % str(msg))
         self.ticks_since_state = 0
-        self.pos = [msg.pose.pose.position.x, msg.pose.pose.position.y]
-        self.vit = [msg.twist.twist.linear.x, msg.twist.twist.linear.y]
+        pos = [msg.pose.pose.position.x, msg.pose.pose.position.y]
+        vit = [msg.twist.twist.linear.x, msg.twist.twist.linear.y]
+        
+        self.robot.pos = coordsToVec(pos)
+        self.robot._v_ = coordsToVec(vit)
         
         qt = Quaternion ( msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w );
         self.orient = quat_to_angle(qt)
-        
+        self.robot.orient_ = Vec(math.sin(self.orient),-math.cos(self.orient))
         self.w_vit = msg.twist.twist.angular.z
-        #rospy.loginfo("-D- odomCallback: orient %.2f (%.2f, %.2f)" %( self.orient, self.pos[0], self.pos[1]))
-        #self.odom_msg = msg
-        #self.pos_est = self.pos = [msg.x, msg.y]
-        #self.orient_est = self.orient = msg.theta
+        #rospy.loginfo("-D- odomCallback: orient %.2f (%.2f, %.2f)" %( self.orient, pos[0], pos[1]))
 
-        
+         
 #############################################################
 #############################################################
 if __name__ == '__main__':
--- rosebot_description/setup.py
+++ rosebot_description/setup.py
@@ -2,11 +2,16 @@
 
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
+import os
+
+os.environ['DISTUTILS_DEBUG'] = "1"
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['rosebot_description'],
-    package_dir={'': 'src'},
+    packages=[ 'aura', 'pygapp'],
+ 	package_dir={'':'src'},
+	#include_package_data=True,
+    package_data={'aura': ['sauvegardes/robomovies.par', 'media/images/robomovies.jpg', 'media/fonts/*'],},
 )
 
 setup(**setup_args)
